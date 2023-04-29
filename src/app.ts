c69dff9961920eff3de211015089ea72737ce063
Aldo Vasquez <aldovasquez2014@gmail.com>
2023-04-28 19:08:07 -0600

    mostrando arbol

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import * as fs from 'fs';
+import { exec } from 'child_process';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
@@ -8,7 +10,7 @@ import Gramatica from './AST/Gramatica';
 import { Tabla } from './Tabla/Tabla';
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
@@ -46,25 +48,37 @@ app.post('/ejecutar', (req, res) => {
     let ast: AST = analizador.Analizar();
 
     //para el arbol
-    /*let raizAST = Gramatica.parse(cadena_codigo);
-    const contenido = `digraph G { 
-        graph [ratio=.548];
-        node [style=filled, shape=circle, width=1
-            fontname=Helvetica, fontweight=bold, fontcolor=black,
-            fontsize=10, fixedsize=true];
-    `+ raizAST.graficar() + " \n }";
-    console.log(contenido);*/
+    try {
+        let raizAST = Gramatica.parse(cadena_codigo);
+        const archivo = 'temp.txt'
+        const contenido = `digraph G { 
+            graph [ratio=.548];
+            node [style=filled, shape=circle, width=1
+                fontname=Helvetica, fontweight=bold, fontcolor=black,
+                fontsize=10, fixedsize=true];
+        `+ raizAST.graficar() + " \n }";
+        console.log(contenido);
 
-    //mostrando la tabla de simbolso
-    Tabla.mostrarTabla();
-    const contenido = `digraph G {
-        node[shape=plaintext]
-        table [label=<<table border="1" cellspacing="0">
-            <tr><td>Identificador</td><td>Tipo</td><td>Tipo</td><td>Linea</td><td>Columna</td></tr>
-            ${Tabla.graficarTabla()}
-        </table>>]
-    }`
-    console.log(contenido);
+        fs.writeFile(archivo, contenido, err => {
+            if (err) {
+                console.error('Falló escribir el archivo ', err);
+            } else console.log('archivo creado correctamente');
+        });
+
+        exec("dot -Tsvg temp.txt -o temp.svg", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    } catch (error) {
+        console.error('Ha ocurrido un error:', error);
+    }
 
     if (ast != undefined) {
         res.render('index.ejs', { title: 'TypeWise', salida: ast.getSalida(), codigo: cadena_codigo });
@@ -72,5 +86,3 @@ app.post('/ejecutar', (req, res) => {
         res.render('index.ejs', { title: 'TypeWise', salida: 'ERROR al procesar cadena', codigo: cadena_codigo });
     }
 });
-
-
